9d31f6899b15471050e97f01adf9c19f1fb06c0b
ko-7 <shimura21ngs@gmail.com>
2020-06-23 19:32:54 +0900

    add title number

--- src/pages/top/Top.vue
+++ src/pages/top/Top.vue
@@ -10,38 +10,38 @@
             <!-- 以下コンテンツ(背景白部分の全体) -->
             <v-card tile class="pa-5" width="100%" height="100%" flat>
                 <!-- ヘッダー -->
-                <p class="font-weight-medium mb-0">【一言メッセージ】</p>
+                <p class="font-weight-medium mb-0">【➀一言メッセージ】</p>
                 <v-divider></v-divider>
                 <v-app-bar color="white" flat class="my-5">
-                    <v-toolbar-title><a href="https://master.d25akkonz7i73i.amplifyapp.com">【ロゴマーク】</a></v-toolbar-title>
+                    <v-toolbar-title><a href="/">【➁ロゴマーク】</a></v-toolbar-title>
                     <v-spacer></v-spacer>
                     <v-toolbar-items>
                         <v-btn text class="px-1">
                             <div>
                                 <v-icon class="mb-3">mdi-account</v-icon>
                                 <v-spacer></v-spacer>
-                                【法人名】とは
+                                【➂法人名】とは
                             </div>
                         </v-btn>
                         <v-btn text class="px-5">
                             <div>
                                 <v-icon class="mb-3">mdi-pencil</v-icon>
                                 <v-spacer></v-spacer>
-                                活動内容
+                                ➃活動内容
                             </div>
                         </v-btn>
                         <v-btn text class="px-3">
                             <div>
                                 <v-icon class="mb-3">mdi-home</v-icon>
                                 <v-spacer></v-spacer>
-                                家を借りる
+                                ➄家を借りる
                             </div>
                         </v-btn>
                         <v-btn text class="px-2">
                             <div>
                                 <v-icon class="mb-3">mdi-home-outline</v-icon>
                                 <v-spacer></v-spacer>
-                                家を活用する
+                                ➅家を活用する
                             </div>
                         </v-btn>
                         <!-- <v-btn text>
@@ -55,7 +55,7 @@
                             <div>
                                 <v-icon class="mb-3">mdi-email</v-icon>
                                 <v-spacer></v-spacer>
-                                お問い合わせ
+                                ➆お問い合わせ
                             </div>
                         </v-btn>
                     </v-toolbar-items>
