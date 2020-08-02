83b5cbd82a53b21c584558090abe91296e107d08
ko-7 <shimura21ngs@gmail.com>
2020-08-02 10:56:42 +0900

    chenged at 8/2

--- src/pages/organization/Organization.vue
+++ src/pages/organization/Organization.vue
@@ -1,57 +1,58 @@
 <template>
   <v-app>
-    <!-- 背景全面を灰色にするためのcard -->
-    <!-- <v-card color="blue-grey lighten-5 text-caption" width="100%" height="100%" flat> -->
     <!-- コンテンツとフッターのラッパー（サイズ調整用） -->
     <v-content>
       <v-container>
         <v-row class="ma-0 pa-0" justify="center">
           <v-col cols="12" sm="10" md="10" lg="8" xl="6" class="mb-5 mx-0 pa-0">
-            <!-- 以下コンテンツ(背景白部分の全体) -->
-            <!-- <v-card tile width="100%" height="100%" flat> -->
                 <!-- ヘッダー -->
-                <p class="font-weight-medium mb-0">【一言メッセージ】</p>
+                  <p class="font-weight-medium mb-0">いわたタウンマネジメント</p>
                 <v-divider color="#BDBDBD"></v-divider>
-                <v-app-bar color="white" flat class="my-5">
-                    <v-toolbar-title><a href="/">【ロゴ】いわたタウンマネジメント</a></v-toolbar-title>
+                <v-app-bar color="white" flat class="my-3">
+                    <v-toolbar-title>
+                      <v-card class="my-1" width="100%" tile flat href="/">
+                        <v-img src="@/assets/logo.jpg"></v-img>
+                      </v-card>
+                    </v-toolbar-title>
                     <v-spacer></v-spacer>
                     <v-toolbar-items>
-                        <v-btn text class="px-5 text-caption" href="/organization.html">
-                            <div>
-                                <v-icon class="mb-3">mdi-account</v-icon>
+                        <v-btn text class="px-3 text-caption" href="/">
+                            <div align="center">
+                                <v-icon class="mb-1">mdi-home</v-icon>
                                 <v-spacer></v-spacer>
-                                  組織
+                                <p class="mb-0">TOP</p>
                             </div>
                         </v-btn>
-                        <!-- <v-btn text class="px-1 text-caption">
-                            <div>
-                                <v-icon class="mb-3">mdi-email</v-icon>
+                        <v-btn text class="px-3 text-caption" href="/organization.html">
+                            <div align="center">
+                                <v-icon class="mb-1" style="color:#FFD700;">mdi-account</v-icon>
                                 <v-spacer></v-spacer>
-                                お問い合わせ
+                                <p class="mb-0">方針</p>
                             </div>
-                        </v-btn> -->
+                        </v-btn>
                     </v-toolbar-items>
                 </v-app-bar>
-
-                <br>
                 
                 <!-- 画像 -->
                 <v-card class="my-5" width="100%" tile>
                   <v-img src="@/assets/organization.png"></v-img>
                 </v-card>
 
+                <v-card flat class="my-5">
+                  <v-divider color="#BDBDBD"></v-divider>
+                    <p class="my-1">今後の活動目標</p>
+                  <v-divider color="#BDBDBD" class="mb-3"></v-divider>
+                  <h4 class="mb-1">磐田の未来創造へ！！</h4>
+                  <p class="mb-0">●学生と店舗、起業とのコラボ</p>
+                  <p class="mb-0">●まちづくりの勉強会</p>
+                  <p class="mb-0">●市民からのイベント提案</p>
+                </v-card>
+
                 <!-- 組織紹介 -->
                 <v-card flat class="my-5">
                   <v-divider color="#BDBDBD"></v-divider>
-                    <p class="my-1">いわたタウンマネジメントとは</p>
-                  <v-divider color="#BDBDBD"></v-divider>
-                  <h4 class="mb-1"></h4>
-                  <p class="mb-0">磐田市内の生活者、商店、企業、行政、他各種団体に対して、中心市街地と郊外市街地の連携と地域の個人・組織の枠を超えた人財の融合によるタウンマネジメントに関する事業を行い、先人が培ってきた歴史や文化、風土を最大限に活かし住みよい地域環境の創造と活力ある地域経済の発展に寄与することを目的とする。</p>
-                </v-card>
-                <v-card flat class="my-5">
-                  <v-divider color="#BDBDBD"></v-divider>
                     <p class="my-1">定款・事業報告書</p>
-                  <v-divider color="#BDBDBD"></v-divider>
+                  <v-divider color="#BDBDBD" class="mb-3"></v-divider>
                   <p class="mb-0">
                     <a href="https://www.npo-homepage.go.jp/npoportal/document/022000922/teikan/1/%E5%AE%9A%E6%AC%BE.pdf" target="_blank">
                       定款＿特定非営利活動法人いわたタウンマネジメント
@@ -68,19 +69,7 @@
                     </a>
                   </p>
                 </v-card>
-                <v-card flat class="my-5">
-                  <v-divider color="#BDBDBD"></v-divider>
-                    <p class="my-1">活動実績</p>
-                  <v-divider color="#BDBDBD"></v-divider>
-                  <h4 class="mb-1"></h4>
-                  <p class="mb-3">1999年1月3日 第一回フリーマーケット</p>
-                  <p class="mb-3">2000年6月5日 第二回フリーマーケット</p>
-                  <p class="mb-3">2001年7月3日 第三回フリーマーケット</p>
-                  <p class="mb-3">2002年8月1日 第四回フリーマーケット</p>
-                  <p class="mb-3">2003年7月8日 第五回フリーマーケット</p>
-
-                </v-card>
-
+                
                 <!-- フッター -->
                 <v-divider class="mt-5" color="#BDBDBD"></v-divider>
                 <v-footer color="white text-center">
@@ -92,12 +81,10 @@
                   </v-container>
                 </v-footer>
 
-            <!-- </v-card> -->
           </v-col>
         </v-row>
       </v-container>
     </v-content>
-    <!-- </v-card> -->
   </v-app>
 </template>
 
--- src/pages/top/Top.vue
+++ src/pages/top/Top.vue
@@ -1,47 +1,41 @@
 <template>
   <v-app>
-    <!-- 背景全面を灰色にするためのcard -->
-    <!-- <v-card color="blue-grey lighten-5 text-caption pa-0 ma-0" width="100%" height="100%" flat> -->
     <!-- コンテンツとフッターのラッパー（サイズ調整用） -->
     <v-content>
       <v-container>
         <v-row class="ma-0 pa-0" justify="center">
           <v-col cols="12" sm="10" md="10" lg="8" xl="6" class="mb-5 mx-0 pa-0">
-            <!-- 以下コンテンツ(背景白部分の全体) -->
-            <!-- <v-card tile width="100%" height="100%" flat> -->
                 <!-- ヘッダー -->
-                <p class="font-weight-medium mb-0">【一言メッセージ】</p>
+                <p class="font-weight-medium mb-0">いわたタウンマネジメント</p>
                 <v-divider color="#BDBDBD"></v-divider>
-                <v-app-bar color="white" flat class="my-5">
+                <v-app-bar color="white" flat class="my-3">
                     <v-toolbar-title>
-                      <a href="/">
-                        【ロゴ】いわたタウンマネジメント
-                      </a>
+                      <v-card class="my-1" width="100%" tile flat href="/">
+                        <v-img src="@/assets/logo.jpg"></v-img>
+                      </v-card>
                     </v-toolbar-title>
                     <v-spacer></v-spacer>
                     <v-toolbar-items>
-                        <v-btn text class="px-5 text-caption" href="/organization.html">
-                            <div>
-                                <v-icon class="mb-3">mdi-account</v-icon>
+                        <v-btn text class="px-3 text-caption" href="/">
+                            <div align="center">
+                                <v-icon class="mb-1" style="color:#FFD700;">mdi-home</v-icon>
                                 <v-spacer></v-spacer>
-                                組織
+                                <p class="mb-0">TOP</p>
                             </div>
                         </v-btn>
-                        <!-- <v-btn text class="px-1 text-caption">
-                            <div>
-                                <v-icon class="mb-3">mdi-email</v-icon>
+                        <v-btn text class="px-3 text-caption" href="/organization.html">
+                            <div align="center">
+                                <v-icon class="mb-1">mdi-account</v-icon>
                                 <v-spacer></v-spacer>
-                                お問い合わせ
+                                <p class="mb-0">方針</p>
                             </div>
-                        </v-btn> -->
+                        </v-btn>
                     </v-toolbar-items>
                 </v-app-bar>
 
-                <br>
-
                 <!-- 画像スライド -->
                 <v-responsive :aspect-ration="1.7">
-                  <v-carousel cycle height="100%" width="100%" hide-delimiter-background :show-arrows="false">
+                  <v-carousel continuous cycle height="100%" width="100%" hide-delimiter-background :show-arrows="false">
                     <v-carousel-item
                       v-for="(item,i) in items"
                       :key="i"
@@ -50,23 +44,31 @@
                   </v-carousel>
                 </v-responsive>
 
+                <!-- 組織紹介 -->
+                <v-card flat class="my-5">
+                  <v-divider color="#BDBDBD"></v-divider>
+                    <p class="my-1">いわたタウンマネジメントとは</p>
+                  <v-divider color="#BDBDBD" class="mb-3"></v-divider>
+                  <h4 class="mb-1"></h4>
+                  <p class="mb-0">磐田市内の生活者、商店、企業、行政、他各種団体に対して、中心市街地と郊外市街地の連携と地域の個人・組織の枠を超えた人財の融合によるタウンマネジメントに関する事業を行い、先人が培ってきた歴史や文化、風土を最大限に活かし住みよい地域環境の創造と活力ある地域経済の発展に寄与することを目的とする。</p>
+                </v-card>
                 <!-- 活動内容紹介 -->
                 <v-card flat class="my-5">
                   <v-divider color="#BDBDBD"></v-divider>
                     <p class="my-1">活動内容</p>
-                  <v-divider color="#BDBDBD"></v-divider>
-                  <h4 class="mb-1">フリーマーケット</h4>
-                  <p class="mb-0">磐田市内でフリーマーケットを企画し、定期的に開催しています。</p>
-                  <p class="mb-0"></p>
+                  <v-divider color="#BDBDBD" class="mb-3"></v-divider>
+                  <p class="mb-0">●ウィークエンドマーケット</p>
+                  <p class="mb-0">●ビアガーデン</p>
+                  <p class="mb-0">●ハロウィンパーティー</p>
                 </v-card>
-                <v-card flat class="my-5">
+                <!-- <v-card flat class="my-5">
                   <v-divider color="#BDBDBD"></v-divider>
                     <p class="my-1">今後の予定</p>
                   <v-divider color="#BDBDBD"></v-divider>
                   <h4 class="mb-1">空き家のリノベーション</h4>
                   <p class="mb-0">ご自身が「暮らすため」あるいは「賃貸や売却で利益を得るため」といった理由から、空き家のリフォーム・リノベーションを検討する方が増えつつあります。「古い空き家を所有し続けることにより、固定資産税が高くなってしまわないか」と不安に思う方もいらっしゃるでしょう。とは言え、リフォームの際には工事費用がいくらかかるか、またどのような工事が必要か、心配な方もきっと多いはず。</p>
                   <p>そこで、私たちいわたタウンマネジメントでそういった方々をサポートしたいと思っています。</p>
-                </v-card>
+                </v-card> -->
 
 
                 <!-- フッター -->
@@ -80,12 +82,10 @@
                   </v-container>
                 </v-footer>
 
-            <!-- </v-card> -->
           </v-col>
         </v-row>
       </v-container>
     </v-content>
-    <!-- </v-card> -->
   </v-app>
 </template>
 
@@ -107,7 +107,13 @@
           {
             src: require('@/assets/activity4.jpg'),
           },
-        ],
+          {
+            src: require('@/assets/activity5.jpg'),
+          },
+          {
+            src: require('@/assets/activity6.jpg'),
+          },
+      ],
     }),
   }
 </script>
