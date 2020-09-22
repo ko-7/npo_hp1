2e9d3f97abf4f99b2918e63c05cb19fae751f051
K.S <shimura21ngs@gmail.com>
2020-09-22 20:19:46 +0900

    delete ContentVshow from management

--- src/pages/management/Management.vue
+++ src/pages/management/Management.vue
@@ -48,24 +48,24 @@
             <v-card flat class="my-5">
               <v-divider color="#BDBDBD" />
                   <!-- {{ Contents }} -->
-                  <v-textarea color="#BDBDBD" class="my-0 py-0" auto-grow rows="1" v-model="Contents[10].name" v-show="ContentVshow[10]" />
-                  <!-- <v-textarea rows="1" v-model="Contents[0].name" v-show="ContentVshow[0]" :placeholder="Contents[0].name"></v-textarea> -->
+                  <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[10].name" />
+                  <!-- <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[0].name" v-show="ContentVshow[0]" /> -->
               <!-- <v-divider color="#BDBDBD" class="mb-3" /> -->
               <!-- <h4 class="mb-1"></h4> -->
-                  <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[11].name" v-show="ContentVshow[11]" :placeholder="Contents[11].name"></v-textarea>
-                  <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[12].name" v-show="ContentVshow[12]" :placeholder="Contents[12].name"></v-textarea> -->
-                  <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[13].name" v-show="ContentVshow[13]" :placeholder="Contents[13].name"></v-textarea> -->
-                  <!-- <v-btn small class="my-0 py-0" color="primary" v-show="ContentVshow[12]" v-on:click="ContentDelete(12)">×行を削除</v-btn> -->
+                  <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[11].name" />
+                  <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[12].name" :placeholder="Contents[12].name"></v-textarea> -->
+                  <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[13].name" :placeholder="Contents[13].name"></v-textarea> -->
+                  <!-- <v-btn small class="my-0 py-0" color="primary" v-on:click="ContentDelete(12)">×行を削除</v-btn> -->
                   <!-- <v-btn small color="primary" v-show="!ContentVshow[12]" v-on:click="ContentAdd(12)">＋行を追加</v-btn> -->
             </v-card>
             <!-- 活動内容紹介 -->
             <v-card flat class="my-5">
               <v-divider color="#BDBDBD" />
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[20].name" v-show="ContentVshow[20]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[20].name" />
               <!-- <v-divider color="#BDBDBD" class="mb-3" /> -->
-                <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[21].name" v-show="ContentVshow[21]" />
-                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[22].name" v-show="ContentVshow[22]" /> -->
-                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[23].name" v-show="ContentVshow[23]" /> -->
+                <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[21].name" />
+                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[22].name" /> -->
+                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[23].name" /> -->
               <!-- <p class="mb-0">●ウィークエンドマーケット</p> -->
               <!-- <p class="mb-0">●ビアガーデン</p> -->
               <!-- <p class="mb-0">●ハロウィンパーティー</p> -->
@@ -73,12 +73,12 @@
 
             <v-card flat class="my-5">
               <v-divider color="#BDBDBD" />
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[30].name" v-show="ContentVshow[30]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[30].name" />
                 <!-- <p class="my-1">今後の活動目標</p> -->
               <!-- <v-divider color="#BDBDBD" class="mb-3" /> -->
-                <h1><v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[31].name" v-show="ContentVshow[31]" /></h1>
-                <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[32].name" v-show="ContentVshow[32]" />
-                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[33].name" v-show="ContentVshow[33]" /> -->
+                <h1><v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[31].name" /></h1>
+                <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[32].name" />
+                <!-- <v-textarea dense class="my-0 mx-3 py-0" auto-grow rows="1" v-model="Contents[33].name" /> -->
               <!-- <h4 class="mb-1">磐田の未来創造へ！！</h4> -->
               <!-- <p class="mb-0">●学生と店舗、起業とのコラボ</p> -->
               <!-- <p class="mb-0">●まちづくりの勉強会</p> -->
@@ -88,32 +88,32 @@
             <!-- 定款・事業報告書 -->
             <v-card flat class="my-5">
               <v-divider color="#BDBDBD" />
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[40].name" v-show="ContentVshow[40]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[40].name" />
                 <!-- <p class="my-1">定款・事業報告書</p> -->
               <!-- <v-divider color="#BDBDBD" class="mb-3" /> -->
               <v-card-actions class="my-0 py-0">
                 <p>➀表示：</p>
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[41].name" v-show="ContentVshow[41]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[41].name" />
               </v-card-actions>
               <v-card-actions class="my-0 pt-0 pb-5">
                 <p>➀URL：</p>
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[42].name" v-show="ContentVshow[42]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[42].name" />
               </v-card-actions>
               <v-card-actions class="my-0 py-0">
                 <p>➁表示：</p>
-                <v-textarea dense class="my-0 pt-5 pb-0" auto-grow rows="1" v-model="Contents[43].name" v-show="ContentVshow[43]" />
+                <v-textarea dense class="my-0 pt-5 pb-0" auto-grow rows="1" v-model="Contents[43].name" />
               </v-card-actions>
               <v-card-actions class="my-0 pt-0 pb-5">
                 <p>➁URL：</p>
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[44].name" v-show="ContentVshow[44]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[44].name" />
               </v-card-actions>
               <v-card-actions class="my-0 py-0">
                 <p>➂表示：</p>
-                <v-textarea dense class="my-0 pt-5 pb-0" auto-grow rows="1" v-model="Contents[45].name" v-show="ContentVshow[45]" />
+                <v-textarea dense class="my-0 pt-5 pb-0" auto-grow rows="1" v-model="Contents[45].name" />
               </v-card-actions>
               <v-card-actions class="my-0 py-0">
                 <p>➂URL：</p>
-                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[46].name" v-show="ContentVshow[46]" />
+                <v-textarea dense class="my-0 py-0" auto-grow rows="1" v-model="Contents[46].name" />
               </v-card-actions>
               <!-- <p class="mb-0">
                 <a href="https://www.npo-homepage.go.jp/npoportal/document/022000922/teikan/1/%E5%AE%9A%E6%AC%BE.pdf" target="_blank">
@@ -161,10 +161,10 @@
   export default {
     data: () => ({
       Contents: [],
-      ContentVshow: [],  //行の表示・非表示を管理する行列
+      // ContentVshow: [],  //行の表示・非表示を管理する行列
       limit: 50,
-      LineCount: [],
-      test: [],
+      // LineCount: [],
+      // test: [],
 
     //↓ スライドに表示させる画像のパス
       items: [
@@ -201,20 +201,21 @@
         this.Contents = _.orderBy(Contents.data.listTodos.items, 'id', 'asc').slice(0, 100)
         for (let i=0; i<50; i++){
           if (this.Contents[i].name === ""){
-            if (i < 10){                          //i=0~9の時は10の位の数字は0とする
-              if (this.LineContent[0] === ""){
-                this.LineContent[0] = i
-              }
-            }else{                                //i < 10の時はLineContentのインデックス番号にContentのインデックス番号の十の位を使う
-              let num = String(i)                 //numten：十の位、numone：一の位
-              let numten = num.substring(0,1)
-              let numone = num.substring(1,2)
-              if(this.LineContent[numten] === ""){
-                this.LineContent[numten] = numone
-              }
-            }
+            // if (i < 10){                          //i=0~9の時は10の位の数字は0とする
+              // if (this.LineContent[0] === ""){
+                // this.LineContent[0] = i
+              // }
+            // }else{                                //i < 10の時はLineContentのインデックス番号にContentのインデックス番号の十の位を使う
+              // let num = String(i)                 //numten：十の位、numone：一の位
+              // let numten = num.substring(0,1)
+              // let numone = num.substring(1,2)
+              // if(this.LineContent[numten] === ""){
+                // this.LineContent[numten] = numone
+              // }
+            // }
+            return
           }
-          this.ContentVshow[i] = true
+          // this.ContentVshow[i] = true
         }
       },
       // 編集したデータをDynamoDBに保存
@@ -244,15 +245,15 @@
       // },
 
       // 表示画面から1行削除
-      ContentDelete: function(id){
-        this.Contents[id].name  = ""
-        this.ContentVshow[id] = false
-      },
+      // ContentDelete: function(id){
+      //   this.Contents[id].name  = ""
+      //   this.ContentVshow[id] = false
+      // },
       // 表示画面に1行追加
-      ContentAdd: function(id){
-        this.ContentVshow[id] = true
-        this.Contents[id].name = "編集してください"
-      }
+      // ContentAdd: function(id){
+      //   this.ContentVshow[id] = true
+      //   this.Contents[id].name = "編集してください"
+      // }
     }
   } 
 </script> 
